7c52197968d44cd18b8f703e6766163648bc4176
chi-hao.lay@outlook.com <development@cschlay.com>
2021-03-27 14:17:17 +0200

    Prepare sandwich fetching in SandwichListView

--- frontend/src/views/SandwichListView.js
+++ frontend/src/views/SandwichListView.js
@@ -1,8 +1,18 @@
+import React, { useEffect, useState } from "react";
+import { mockSandwichList } from "../mocks/sandwiches";
+import { SandwichService } from "../services/SandwichService";
+
 export const SandwichListView = () => {
+    const [sandwiches, setSandwiches] = useState(mockSandwichList);
+
+    /*useEffect(() => {
+        SandwichService.getListing().then(setSandwiches);
+    }, []);*/
+
     return (
         <p>
             <h1>Sandwiches</h1>
-            Sandwich List
+            {JSON.stringify(sandwiches)}
         </p>
     );
 };
