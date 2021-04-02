e168f38f517919bc1e661cdbdd85dfe2c078d913
chi-hao.lay@outlook.com <development@cschlay.com>
2021-04-02 11:09:02 +0300

    Add basic structure for sandwich

---
+++ frontend/src/components/Sandwich/Sandwich.js
@@ -0,0 +1,25 @@
+import styles from "./Sandwich.module.css";
+
+export const Sandwich = ({ id, name, toppings, breadType, onOrder }) => {
+    return (
+        <div>
+            <h2>{name}</h2>
+            <p>Bread type: {breadType}</p>
+            <ToppingList toppings={toppings} />
+            <button onClick={onOrder} data-sandwich-id={id}>Order</button>
+        </div>
+    );
+};
+
+const ToppingList = ({ toppings }) => {
+    return (
+        <>
+            <h3>Toppings</h3>
+            <ul>
+                {toppings.map(data => <Topping key={data.id} {...data} />)}
+            </ul>
+        </>
+    );
+};
+
+const Topping = ({ name }) => <li>{name}</li>;
--- frontend/src/views/SandwichListView.js
+++ frontend/src/views/SandwichListView.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
+import {Sandwich} from "../components/Sandwich/Sandwich";
 import { mockSandwichList } from "../mocks/sandwiches";
 import { SandwichService } from "../services/SandwichService";
 
 export const SandwichListView = () => {
     const [sandwiches, setSandwiches] = useState(mockSandwichList);
+
+    const handleOrder = () => {
+        console.log("User");
+    };
 
     /*useEffect(() => {
         SandwichService.getListing().then(setSandwiches);
@@ -12,7 +17,7 @@ export const SandwichListView = () => {
     return (
         <div>
             <h1>Sandwiches</h1>
-            {JSON.stringify(sandwiches)}
+            {sandwiches.map(data => <Sandwich key={data.id} {...data} onOrder={handleOrder}/>)}
         </div>
     );
 };
