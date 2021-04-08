cd6bd969a68111eeefc56ddef0d66db98227631f
Asbah-Usmani <asbahamjad.usmani@tuni.fi>
2021-04-08 11:42:25 +0300

    mid project check in

--- backend/server-a/index.js
+++ backend/server-a/index.js
@@ -6,17 +6,16 @@ import orderRoutes from './routes/order.js';
 
 const app = express();
 
-app.use('/order', orderRoutes);
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded());
-/*
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))*/
-app.use(cors());
+
+app.use('/order', orderRoutes);
+
 
 const CONNECTION_URL = 'mongodb+srv://asbah:asbah156@cluster0.fwf42.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
