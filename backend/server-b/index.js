cd784971f4404d17f6df41ead213ad8ac5c8ef68
Chi-Hao Lay <chi-hao.lay@tuni.fi>
2021-04-22 20:01:06 +0300

    dockerize server-b

--- backend/server-b/index.js
+++ backend/server-b/index.js
@@ -1,14 +1,14 @@
 const express = require("express")
-const { getTask } = require("./rabbit-utils/receiveTask");
+const taskReceiver = require("./rabbit-utils/receiveTask");
 
 const app = express()
 const PORT = 8000
-const RABBIT_HOST = "localhost:5672"
+const RABBIT_HOST = "rabbitmq:5672"
 const QUEUE_A = "queue_a"
 const QUEUE_B = "queue_b"
 
+taskReceiver.getTask(RABBIT_HOST, QUEUE_A)
+
 app.listen(PORT, () => {
     console.log(`Server B: http://localhost:${PORT}`)
-
-    getTask(RABBIT_HOST, QUEUE_A)
 })
